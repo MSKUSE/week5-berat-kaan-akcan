b0465b1702b329a61e6557ccc062cb1f2ed1f3db
berat-kaan-akcan <beratkaanakcan@posta.mu.edu.tr>
2025-03-20 13:15:44 +0300

    week5

---
+++ week5/src/Circle.java
@@ -0,0 +1,33 @@
+public class Circle {
+
+
+    private Point center;
+    private int radius;
+
+    public Circle(Point center ,int radius) {
+        this.radius = radius;
+        this.center = center;
+    }
+
+    public Point getCenter() {
+        return center;
+    }
+
+    public void setCenter(Point center) {
+        this.center = center;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        if (radius < 0) {
+            this.radius = 0;
+            System.out.println("radius cant be  negative");
+        }else {
+            this.radius = radius;
+        }
+        this.radius = radius;
+    }
+}
